05d61d40d5a81a7ff592df07457486595b063cb0
Adam <maxsuspl@gmail.com>
2024-05-01 16:30:27 +0200

    map generator/ point

---
+++ Game/Map/MapDisplay.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+#func _ready():
+	#queue_redraw()
+
+const margin = 7
+
+var children: Array = []
+
+func add_child_event(child):
+	if !children.has(child):
+		children.append(child)
+		queue_redraw()
+
+func _draw():
+	draw_circle(Vector2.ZERO, 4, Color.WHITE_SMOKE)
+	
+	for child in children:
+		var line = child.position - position
+		var normal = line.normalized()
+		line -= margin * normal
+		var color = Color.GRAY
+		draw_line(normal * margin, line, color, 2, true)
